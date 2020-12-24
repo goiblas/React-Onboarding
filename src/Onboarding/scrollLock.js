4eca45b7c2a94f4d786b641006e2ed4b6f78780b
Jesús Olazagoitia <jesus.designer@gmail.com>
2020-12-25 00:21:29 +0100

    rename disable classname

--- src/Onboarding/scrollLock.js
+++ src/Onboarding/scrollLock.js
@@ -1,6 +1,6 @@
 const style = document.createElement('style')
-style.innerHTML = '.disable-scroll { overflow: hidden }'
+style.innerHTML = '._onboarding-disable-scroll { overflow: hidden }'
 document.getElementsByTagName('head')[0].appendChild(style)
 
-export const disableScroll = () => document.body.classList.add("disable-scroll")
-export const enableScroll = () => document.body.classList.add("disable-scroll")
+export const disableScroll = () => document.body.classList.add("_onboarding-disable-scroll")
+export const enableScroll = () => document.body.classList.remove("_onboarding-disable-scroll")
